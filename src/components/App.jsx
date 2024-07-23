9b010f84af3e92423f13a3fc1d8c5fff85a217fa
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-23 14:46:52 +0300

    feat(app): route settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,20 @@
+import {Navigate, Route, Routes} from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
+import CatalogPage from "../pages/CatalogPage/CatalogPage";
+import FavoritePage from "../pages/FavoritePage/FavoritePage";
+import Layout from "./Layout/Layout";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorite" element={<FavoritePage />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
