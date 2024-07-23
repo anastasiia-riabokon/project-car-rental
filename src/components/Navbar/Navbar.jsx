e46031c6d8244409926630f61258eaeda1d1b0f6
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-23 14:54:56 +0300

    feat(app): add Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,12 @@
+import {NavLink} from "react-router-dom";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <header>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/catalog">Catalog</NavLink>
+      <NavLink to="/favorite">Favorite</NavLink>
+    </header>
+  );
 };
 export default Navbar;
