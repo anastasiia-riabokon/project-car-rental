ef999df10a8785c8b7672146ad1febc9da134638
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-24 12:18:29 +0300

    feat(app): add sketchy card

---
+++ src/components/CarCard/CarCard.jsx
@@ -0,0 +1,24 @@
+const CarCard = (props) => {
+  const {photoLink, address, type, rentalPrice, year, id, model, make} = props;
+  console.log(props);
+  return (
+    <>
+      <img src={photoLink} alt={`${make} ${model}`} width={274} />
+      <span>
+        <p>
+          {make} {model}, {year}
+        </p>
+        <p>{rentalPrice}</p>
+      </span>
+      <span>
+        <p>{address}</p>
+        <p>{type}</p>
+        <p>{year}</p>
+        <p>{id}</p>
+        <p>{model}</p>
+        <p>{make}</p>
+      </span>
+    </>
+  );
+};
+export default CarCard;
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,4 +1,27 @@
+import {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchCarsThunk} from "../../redux/car/operations";
+import {selectCar} from "../../redux/car/selectors";
+import CarCard from "../../components/CarCard/CarCard";
+
 const CatalogPage = () => {
-  return <div>CatalogPage</div>;
+  const [page, setPage] = useState(1);
+  const cars = useSelector(selectCar);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCarsThunk({page: page}));
+  }, [dispatch, page]);
+  return (
+    <div>
+      <ul>
+        {cars.map((item) => (
+          <li key={item.id}>
+            <CarCard {...item} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default CatalogPage;
