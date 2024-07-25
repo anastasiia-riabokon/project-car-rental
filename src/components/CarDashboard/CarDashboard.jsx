8f985760b7faeb19994067fe6f71569d576ba515
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-25 14:44:52 +0300

    feat(app): filterying by priceRental

--- src/components/CarDashboard/CarDashboard.jsx
+++ src/components/CarDashboard/CarDashboard.jsx
@@ -1,13 +1,19 @@
 import Select from "react-dropdown-select";
 import makes from "../../data/makes.json";
 import {Controller, useForm} from "react-hook-form";
-import {useDispatch} from "react-redux";
-import {changeMakeFilter} from "../../redux/filter/slice";
+import {useDispatch, useSelector} from "react-redux";
+import {changeMakeFilter, changeRentalPriceFilter} from "../../redux/filter/slice";
+import {selectCar} from "../../redux/car/selectors";
+import {useEffect, useState} from "react";
+import {fetchCarsThunk} from "../../redux/car/operations";
 
 const CarDashboard = () => {
+  const cars = useSelector(selectCar);
+
   const {control, handleSubmit, setValue} = useForm({
     defaultValues: {
       make: "",
+      rentalPrice: "",
     },
   });
 
@@ -15,13 +21,25 @@ const CarDashboard = () => {
 
   const optionsMake = makes.map((item) => ({value: item, label: item}));
 
+  const prices = [];
+  for (let price = 20; price <= 300; price += 5) {
+    prices.push(price);
+  }
+
+  const optionsRentalPrice = prices.map((item) => ({label: item, value: item}));
+
   const changeMake = (selected) => {
     setValue("make", selected[0]?.value || "");
+  };
+
+  const changeRentalPrice = (selected) => {
+    setValue("rentalPrice", selected[0]?.value || "");
   };
 
   const onSubmit = (data) => {
     console.log(data);
     dispatch(changeMakeFilter(data.make));
+    dispatch(changeRentalPriceFilter(data.rentalPrice.toString()));
   };
 
   return (
@@ -35,6 +53,18 @@ const CarDashboard = () => {
             options={optionsMake}
             placeholder="Enter the text"
             onChange={changeMake}
+          />
+        )}
+      />
+      <Controller
+        name="rentalPrice"
+        control={control}
+        render={({field}) => (
+          <Select
+            {...field}
+            options={optionsRentalPrice}
+            placeholder="To $"
+            onChange={changeRentalPrice}
           />
         )}
       />
--- src/redux/filter/selectors.js
+++ src/redux/filter/selectors.js
@@ -8,10 +8,13 @@ const selectMileageFilter = (state) => state.filter.mileage;
 export const selectFilteredCars = createSelector(
   [selectCar, selectRentalPriceFilter, selectMakeFilter, selectMileageFilter],
   (cars, rentalPrice, make, mileage) => {
+    const rentalPriceNum = parseFloat(rentalPrice) || 0;
+
     return cars.filter((car) => {
-      const rentalPriceCondition = rentalPrice
-        ? car.rentalPrice <= rentalPrice.replace("$", "")
-        : true;
+      const carRentalPriceNum = parseFloat(car.rentalPrice.replace("$", "")) || 0;
+
+      const rentalPriceCondition = rentalPriceNum ? carRentalPriceNum <= rentalPriceNum : true;
+
       const makeCondition = make ? car.make === make : true;
       const mileageCondition = car.mileage >= mileage.min && car.mileage <= mileage.max;
       return rentalPriceCondition && makeCondition && mileageCondition;
