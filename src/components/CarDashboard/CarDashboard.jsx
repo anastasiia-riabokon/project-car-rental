4a5962d93c8f62d451d634f84ffc78451f9c164f
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-25 13:37:03 +0300

    feat(app): filterying by make

--- src/components/CarDashboard/CarDashboard.jsx
+++ src/components/CarDashboard/CarDashboard.jsx
@@ -1,7 +1,45 @@
+import Select from "react-dropdown-select";
 import makes from "../../data/makes.json";
+import {Controller, useForm} from "react-hook-form";
+import {useDispatch} from "react-redux";
+import {changeMakeFilter} from "../../redux/filter/slice";
 
 const CarDashboard = () => {
-  const optionsBrand = makes.map((item) => ({value: item, label: item}));
-  return <div></div>;
+  const {control, handleSubmit, setValue} = useForm({
+    defaultValues: {
+      make: "",
+    },
+  });
+
+  const dispatch = useDispatch();
+
+  const optionsMake = makes.map((item) => ({value: item, label: item}));
+
+  const changeMake = (selected) => {
+    setValue("make", selected[0]?.value || "");
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+    dispatch(changeMakeFilter(data.make));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="make"
+        control={control}
+        render={({field}) => (
+          <Select
+            {...field}
+            options={optionsMake}
+            placeholder="Enter the text"
+            onChange={changeMake}
+          />
+        )}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
 };
 export default CarDashboard;
