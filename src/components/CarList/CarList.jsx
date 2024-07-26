97597de5d07bb07c8d00743eed69aa715d53c95a
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-26 13:54:37 +0300

    feat(app): add and remove car to favorites

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,11 +1,34 @@
+import {selectItemsFavorite} from "../../redux/favorite/selectors";
+import {addCar, removeCar} from "../../redux/favorite/slice";
+import {useDispatch, useSelector} from "react-redux";
+
 import CarCard from "../CarCard/CarCard";
+import IconHeart from "../IconHeart/IconHeart";
 
 const CarList = ({filteredCars}) => {
+  const dispatch = useDispatch();
+  const favCar = useSelector(selectItemsFavorite);
+
+  const handleAddOrRemoveToFavorites = (car) => {
+    const alreadyFavorite = favCar.some((item) => item.id === car.id);
+    if (alreadyFavorite) {
+      const favItem = favCar.find((item) => item.id === car.id);
+      dispatch(removeCar(favItem.idFav));
+    } else {
+      dispatch(addCar(car));
+    }
+  };
   return (
     <div>
       <ul>
         {filteredCars.map((item) => (
           <li key={item.id}>
+            <button type="button" onClick={() => handleAddOrRemoveToFavorites(item)}>
+              <IconHeart
+                fill={favCar.some((fav) => fav.id === item.id) ? "blue" : "none"}
+                stroke={favCar.some((fav) => fav.id === item.id) ? "blue" : "white"}
+              />
+            </button>
             <CarCard {...item} />
           </li>
         ))}
--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,4 +1,20 @@
+import {useSelector} from "react-redux";
+import {selectItemsFavorite} from "../../redux/favorite/selectors";
+import CarCard from "../../components/CarCard/CarCard";
+
 const FavoritePage = () => {
-  return <div>FavoritePage</div>;
+  const favCar = useSelector(selectItemsFavorite);
+  console.log(favCar);
+  return (
+    <div>
+      <ul>
+        {favCar.map((item) => (
+          <li key={item.idFav}>
+            <CarCard {...item} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default FavoritePage;
