f21e0f17bbd68c860a8cc4b5cac4fffe31e19f1c
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-27 08:30:34 +0300

    fix(app): change padding

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,4 +1,4 @@
 const Container = ({children}) => {
-  return <div className="max-w-[1440px] mx-auto px-[32px] min-[1024px]:px-[128px]">{children}</div>;
+  return <div className="max-w-[1440px] mx-auto px-[32px]">{children}</div>;
 };
 export default Container;
