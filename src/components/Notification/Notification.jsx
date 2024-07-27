087afa1057b4e36736a26f7d5f7b338275cddd68
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-27 10:38:04 +0300

    feat(app): add Notification

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,4 @@
+const Notification = ({text}) => {
+  return <div className="text-xl text-center mt-8 font-semibold">{text}</div>;
+};
+export default Notification;
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -9,6 +9,7 @@ import CarDashboard from "../../components/CarDashboard/CarDashboard";
 import CarList from "../../components/CarList/CarList";
 import BtnLoadMore from "../../components/BtnLoadMore/BtnLoadMore";
 import {clearItems} from "../../redux/car/slice";
+import Notification from "../../components/Notification/Notification";
 
 const CatalogPage = () => {
   const [page, setPage] = useState(1);
@@ -18,11 +19,13 @@ const CatalogPage = () => {
 
   useEffect(() => {
     dispatch(fetchCarsThunk({page: page}));
+  }, [dispatch, page]);
 
+  useEffect(() => {
     return () => {
       dispatch(clearItems());
     };
-  }, [dispatch, page]);
+  }, [dispatch]);
 
   const handleClick = () => {
     setPage((prev) => prev + 1);
@@ -30,6 +33,7 @@ const CatalogPage = () => {
   return (
     <div>
       <CarDashboard />
+      {filteredCars.length === 0 && <Notification text="Sorry! Result not found..." />}
       <CarList filteredCars={filteredCars} />
       {showLoadMore && <BtnLoadMore onClick={handleClick} />}
     </div>
