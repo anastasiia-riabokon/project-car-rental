bc5af8eda5f58992c792a2b0fc45e618b0cb1c0f
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-27 14:33:13 +0300

    feat(app): btn load more styled

--- src/components/BtnLoadMore/BtnLoadMore.jsx
+++ src/components/BtnLoadMore/BtnLoadMore.jsx
@@ -1,6 +1,10 @@
 const BtnLoadMore = ({onClick}) => {
   return (
-    <button type="button" onClick={onClick}>
+    <button
+      type="button"
+      onClick={onClick}
+      className="text-[#3470FF] leading-6 font-medium underline hover:text-[#0B44CD] mx-auto block"
+    >
       Load More
     </button>
   );
