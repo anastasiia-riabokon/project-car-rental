c6d9e8d295796521947e6a0125fbe4ee31c39bb9
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-28 10:06:05 +0300

    feat(app): add scrolling page

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,6 +1,7 @@
 import {selectItemsFavorite} from "../../redux/favorite/selectors";
 import {addCar, removeCar} from "../../redux/favorite/slice";
 import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useRef} from "react";
 
 import CarCard from "../CarCard/CarCard";
 import IconHeart from "../IconHeart/IconHeart";
@@ -8,6 +9,8 @@ import IconHeart from "../IconHeart/IconHeart";
 const CarList = ({filteredCars}) => {
   const dispatch = useDispatch();
   const favCar = useSelector(selectItemsFavorite);
+  const carRef = useRef(null);
+  const prevFilteredCarsRef = useRef(filteredCars);
 
   const handleAddOrRemoveToFavorites = (car) => {
     const alreadyFavorite = favCar.some((item) => item.id === car.id);
@@ -18,11 +21,29 @@ const CarList = ({filteredCars}) => {
       dispatch(addCar(car));
     }
   };
+
+  useEffect(() => {
+    if (prevFilteredCarsRef.current !== filteredCars && !prevFilteredCarsRef.current.length) {
+      prevFilteredCarsRef.current = filteredCars;
+      return;
+    }
+
+    if (carRef.current) {
+      const heightEl = carRef.current.getBoundingClientRect().height;
+      window.scrollBy({
+        top: heightEl * 1.5,
+        behavior: "smooth",
+      });
+    }
+
+    prevFilteredCarsRef.current = filteredCars;
+  }, [filteredCars]);
+
   return (
     <div className="pt-[50px] pb-[100px]">
       <ul className="flex gap-y-[50px] gap-x-[29px] flex-wrap justify-center">
-        {filteredCars.map((item) => (
-          <li key={item.id} className="relative">
+        {filteredCars.map((item, index) => (
+          <li key={item.id} className="relative" ref={carRef}>
             <button
               type="button"
               onClick={() => handleAddOrRemoveToFavorites(item)}
