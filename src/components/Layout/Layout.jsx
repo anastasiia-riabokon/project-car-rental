9335cd6bcdea92f9ac0ea471f76afd61b5241198
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-28 12:21:32 +0300

    fix(app): footer usually bottom

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,21 +7,23 @@ import Loader from "../Loader/Loader";
 
 const Layout = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <header className="bg-[#3470FF]">
         <Container>
           <Navbar />
         </Container>
       </header>
 
-      <Suspense fallback={<Loader />}>
-        <Outlet />
-      </Suspense>
+      <main className="flex-grow">
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
 
       <footer className="bg-[#3470FF] text-white text-center p-4">
         <p>&copy; 2024 Car Rental Ukraine. All rights reserved.</p>
       </footer>
-    </>
+    </div>
   );
 };
 export default Layout;
